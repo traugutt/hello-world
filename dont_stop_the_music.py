9199928cc2c1d1a04cb0992d264a9c3e52d38fd6
sofsym <sofsofsymko@gmail.com>
2018-12-26 15:11:32 +0200

    Update dont_stop_the_music.py

--- dont_stop_the_music.py
+++ dont_stop_the_music.py
@@ -1,25 +1,3 @@
-# import requests
-# import time
-# from lxml import html
-# web_page = requests.get("https://www.lyrics.com/artist/Bon%20Iver/991558")
-# tree = html.fromstring(web_page.text)
-# songs = tree.xpath('//tbody//td//a//@href')
-# for item in songs:
-#     r = requests.get("https://www.lyrics.com" + str(item))
-# for n in r:
-#     n.get('//body//pre')
-# for i in n:
-#     time.sleep(1)
-#     with open(Bon_Iver.txt, mode='w', encoding='utf-8') as bon_iver_file:
-#       bon_iver_file.write(i)
-#       time.sleep(3)
-# web_page = requests.get("https://www.lyrics.com/artist/Beyonc%C3%A9/349078")
-# tree = html.fromstring(web_page.text)
-# songs_urls = tree.xpath("//tbody//td//a[starts-with(@href, '/lyric')]//@href")
-# for item in songs_urls:
-#     r = requests.get("https://www.lyrics.com" + str(item))
-#     tree2 = html.fromstring(r.text)
-#     songs = " ".join(tree2.xpath('//body//pre//text()'))
 import requests
 import time
 from lxml import html
