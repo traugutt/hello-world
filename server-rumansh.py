e1b54cef237cd124cbf891787075f89755817f1d
sofsym <sofsofsymko@gmail.com>
2019-05-06 11:28:35 +0300

    Add files via upload

---
+++ server-rumansh.py
@@ -0,0 +1,40 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib
+
+
+class S(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        text = urllib.parse.unquote(self.path[1:])
+        print(text)
+        self.wfile.write(str(text).encode("utf-8"))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("posted".encode("utf-8"))
+
+
+def run(server_class=HTTPServer, handler_class=S, port=8081):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Server started on port ' + str(port))
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
